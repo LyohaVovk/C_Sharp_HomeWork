1d7e134c9c73df2be2e345c974884d73f989b960
Aleks <68148928+LyohaVovk@users.noreply.github.com>
2023-08-11 18:26:56 +0300

    Add task DZ_02_02

---
+++ DZ_02_02.cs
@@ -0,0 +1,42 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+// Функция ввода числа от пользователя
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+// Функция выделения 3 цифры из заданного числа
+void ThirdPushDigit(int num)
+{
+    switch (num)
+    {
+        // Если число меньше 100
+        case < 100:
+            // Выводим сообщение
+            Console.WriteLine($"Число {num} не имеет третьей цифры");
+            break;
+            // если больше вычисляем и выводим
+        case < 1000:
+            Console.WriteLine($"Третья цифра в числе {num} => {num % 10}");
+            break;
+        case < 10000:
+            Console.WriteLine($"Третья цифра в числе {num} => {num % 100 / 10}");
+            break;
+        case < 100000:
+            Console.WriteLine($"Третья цифра в числе {num} => {num % 1000 / 100}");
+            break;
+    }
+}
+
+// Получаем число от пользователя
+int specifiedNumber = InputNum("Введите члюбое число от 0 до 100000: ");
+// Выводит результат работы функции
+ThirdPushDigit(specifiedNumber);
