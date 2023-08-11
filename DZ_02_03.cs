1dadaa02438ef887cedf1cafdaba1985ff184086
Aleks <68148928+LyohaVovk@users.noreply.github.com>
2023-08-11 18:27:35 +0300

    Add task DZ_02_03

---
+++ DZ_02_03.cs
@@ -0,0 +1,36 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+// Функция ввода числа от пользователя
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+// Функция возвращающая истину, если заданный день 6 или 7
+// и ложь, если другое число 
+bool WeekEnd(int num)
+{
+    return num >= 6 && num < 8;
+}
+
+// Получаем число от пользователя
+int dayOfWeek = InputNum("Введите число от 1 до 7, означающее день недели: ");
+// Проверяем на принадлежность отрезку недели
+if (dayOfWeek < 1 || dayOfWeek > 7)
+    Console.WriteLine($"В неделе 7 дней, а не {dayOfWeek} :)");
+else
+{
+    //вычисляем: выходной или нет и выводим результат
+    if (WeekEnd(dayOfWeek))
+        Console.WriteLine($"{dayOfWeek} день недели является выходным");
+    else
+        Console.WriteLine($"{dayOfWeek} день недели является рабочим");
+}
