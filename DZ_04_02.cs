a75ea9dbd2fe03596df960e72455fe7018bce53a
Aleks <68148928+LyohaVovk@users.noreply.github.com>
2023-08-31 23:19:14 +0300

    Задача 27 к семинару 4

---
+++ DZ_04_02.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int CountDigit(int number) 
+{
+    int count = 0;
+    while (number != 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+
+int SumOfDigits(int number, int countDigit)
+{
+    int sum = 0;
+    for(int i = 0; i <= countDigit; i++)
+    {
+        sum += (number % 10);
+        number /= 10;
+    }
+    return sum;
+}
+
+int num = GetNumber("Введите число");
+int sum = SumOfDigits(num, CountDigit(num));
+Console.WriteLine($"Сумма цифр числа {num} => {sum}");
