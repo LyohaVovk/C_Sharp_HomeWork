0ab184468808d2821b31855c9a690246a7cdf243
Aleks <68148928+LyohaVovk@users.noreply.github.com>
2023-08-31 23:19:59 +0300

    Задача 29 к семинару 4

---
+++ DZ_04_03.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] InputArray(string message)
+{
+    int [] array = new int [8];
+    for(int i = 0; i < 8; i++)
+    {
+        Console.WriteLine(message);
+        array[i] = int.Parse(Console.ReadLine())!;
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"{array [i]} ");
+    }
+} 
+int [] array = InputArray("Введите новый элемент массива: ");
+PrintArray(array);
