949e37eaba92417448dd98ae2cf995dace8b4cf9
Aleks <68148928+LyohaVovk@users.noreply.github.com>
2023-09-07 22:21:06 +0300

    Добавлена задача №41 к семинару 6

---
+++ DZ_06_01.cs
@@ -0,0 +1,27 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int PosQuantityNum(int number)
+{
+     int result = 0;
+     int count = 1;
+     while (count <= number)
+    {
+        int num = GetNumber($"Введите {count} число: ");
+        if (num > 0)
+            result++;
+        count++;    
+    }
+    return result;
+}
+
+int result = PosQuantityNum(GetNumber("Введите количество задаваемых чисел: "));
+Console.WriteLine($"Вы ввели {result} положительных чисел");
