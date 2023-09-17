27215e0dfd63a9cff592118fa6819941d95d66c7
Aleks <68148928+LyohaVovk@users.noreply.github.com>
2023-09-17 22:46:50 +0300

    Добавлена задача №56 к семинару 8

---
+++ DZ_08_02.cs
@@ -0,0 +1,75 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+// с наименьшей суммой элементов: 1 строка
+
+// int GetNumber(string message)
+// {
+//     Console.Write(message);
+//     return int.Parse(Console.ReadLine()!);
+// }
+
+int [,] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRandom)
+{
+    int [,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = rand.Next(minLimitRandom, maxLimitRandom + 1);
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [] RowSum(int [,] matrix)
+{
+    int [] sumElem = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) sumElem[i] += matrix[i, j];
+    }
+    return sumElem;
+}
+
+void SmallestSum(int [] sumArray)
+{
+    int index = 0;
+    int small = sumArray[0];
+    for (int i = 1; i < sumArray.Length; i++)
+    {
+        if (sumArray[i] < small)
+        {
+            small = sumArray[i];
+            index = i;
+        }
+    }
+    Console.WriteLine($"Наименьшая сумма элементов => {small} в строке {index + 1}");
+}
+
+int [,] matrix = CreateRandomMatrix(4, 4, 0, 10);
+Console.WriteLine("Заданный массив:");
+PrintMatrix(matrix);
+Console.WriteLine();
+int [] sumArray = RowSum(matrix);
+Console.WriteLine("Сумма строк:");
+for (int i = 0; i < sumArray.Length; i++) Console.WriteLine($"{sumArray[i]}");
+Console.WriteLine();
+SmallestSum(sumArray);
